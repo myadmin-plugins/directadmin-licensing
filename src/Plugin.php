b2bb306f74d61b8afadb088d99b2b7bac0925d6d
Joe Huss <detain@interserver.net>
2017-06-24 15:09:16 -0400

    a bunch of updates improving the code making use of self:: and other imprvoements

--- src/Plugin.php
+++ src/Plugin.php
@@ -72,9 +72,9 @@ class Plugin {
 		$menu = $event->getSubject();
 		$module = self::$module;
 		if ($GLOBALS['tf']->ima == 'admin') {
-			$menu->add_link($module, 'choice=none.reusable_directadmin', 'icons/database_warning_48.png', 'ReUsable Directadmin Licenses');
-			$menu->add_link($module, 'choice=none.directadmin_list', 'icons/database_warning_48.png', 'Directadmin Licenses Breakdown');
-			$menu->add_link($module.'api', 'choice=none.directadmin_licenses_list', 'whm/createacct.gif', 'List all Directadmin Licenses');
+			$menu->add_link(self::$module, 'choice=none.reusable_directadmin', 'icons/database_warning_48.png', 'ReUsable Directadmin Licenses');
+			$menu->add_link(self::$module, 'choice=none.directadmin_list', 'icons/database_warning_48.png', 'Directadmin Licenses Breakdown');
+			$menu->add_link(self::$module.'api', 'choice=none.directadmin_licenses_list', 'whm/createacct.gif', 'List all Directadmin Licenses');
 		}
 	}
 
